f283f7db5df46eb34e2247fc91ada0e257f19d24
Zackory Cramer <zackory137c@gmail.com>
2018-06-20 18:31:46 -0700

    Initial Commit

---
+++ Flat Land ServerTests/Flat_Land_ServerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Flat_Land_ServerTests.swift
+//  Flat Land ServerTests
+//
+//  Created by Zackory Cramer on 6/20/18.
+//  Copyright © 2018 Zackory Cui. All rights reserved.
+//
+
+import XCTest
+@testable import Flat_Land_Server
+
+class Flat_Land_ServerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
